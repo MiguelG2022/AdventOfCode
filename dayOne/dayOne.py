250624e2af42254ec08b7715870fa3e545d8ad25
MiguelG2022 <90057533+MiguelG2022@users.noreply.github.com>
2022-12-25 00:02:45 -0800

    created folder for dayOne which contains the solution and puzzle input

---
+++ dayOne/dayOne.py
@@ -0,0 +1,37 @@
+# Reads data from Advent of Code 
+with open("dayOneInput.txt", "r") as day3:
+	lines = day3.readlines()
+
+# Removes \n from data and also converts string into integers 
+values = []
+for i in lines: 
+    stripped = i.replace("\n","")
+    # values.append(stripped)
+    if len(stripped)>0:
+        values.append(int(stripped))
+    else:
+        values.append(stripped)
+  
+
+def getElfCalories(nums):
+    count = 0
+    res = []
+    for num in nums:
+        if num == "":
+            res.append(count)
+            count = 0
+        else:
+            count+=num 
+    return res    
+calories = getElfCalories(values)
+def getElfMax(nums):
+    return max(nums)
+
+def getTopThree(nums):
+    nums.sort(reverse=True)
+    return nums[0] + nums[1] + nums[2]
+
+print(getElfMax(calories))
+print(getTopThree(calories))
+
+
